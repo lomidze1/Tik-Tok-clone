7279a17a807f4f37a693612fc1caded74c8533ed
Giorgi Lomidze <99074835+lomidze1@users.noreply.github.com>
2022-12-18 13:43:31 +0400

    Add files via upload

---
+++ tik-tok_clone/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com'
+    ],
+  },
+};
+
+module.exports = nextConfig;
